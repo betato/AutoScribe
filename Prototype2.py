a8695c4d0ced71d33c32353224ea9a343aa89076
JenniferTrac <jtrac019@uottawa.ca>
2019-11-12 10:40:22 -0500

    not my proudest work tbh

--- Prototype2.py
+++ Prototype2.py
@@ -7,9 +7,11 @@ Goal:
 '''
 
 import Prototype1
+import emailer
 import math
 import pygame as pg
 import csv
+import time
 
 # CONSTANTS
 white = (255,255,255)
@@ -19,7 +21,7 @@ file_name = "whiteboard.jpg"
 
 # FUNCTIONS
 def draw_current():
-    '''() -> ()
+    '''() -> (list of tuples)
     Reads file of string lengths and adds current pen stroke to already existing canvas
     Saves the file'''
     with open('lengths.txt') as file:
@@ -29,18 +31,38 @@ def draw_current():
             coordinates.append(Prototype1.get_coordinate((float(row[0]),float(row[1]))))
     return(coordinates)
 
+def get_email():
+    '''() -> (String)
+    Reads DashboardInfo.txt and returns email inside
+    '''
+    with open('DashboardInfo.txt') as file:
+        reader = csv.reader(file, delimiter = ',')
+    return(reader[0][1])
+
 # MAIN
 canvas = pg.display.set_mode((Prototype1.X_PIXELS,Prototype1.Y_PIXELS))
 canvas.fill(white)
 
-pg.draw.lines(canvas, black, False, draw_current(), pen_thickness)
-pg.image.save(canvas,file_name)
+for i in range(0,4): #change this loop to while true when actually using the device
+    pg.draw.lines(canvas, black, False, draw_current(), pen_thickness)
+    pg.image.save(canvas,file_name)
 
+    dash_file = open('DashboardInfo.txt', 'r')
+    dash_info = []
+    for i in range (0,3):
+        dash_info.append(dash_file.readline())
+        #0 for dont send, 1 for send
+        #email to send to
+        #clear screen?
+    dash_file.close()
 
+    print('1' in dash_info[0])
 
-
-
-
-
-
-#########################################
+    if '1' in dash_info[0]:
+        emailer.send_email(dash_info[1])
+    if '1' in dash_info[2]:
+        canvas.fill(white)
+        pg.image.save(canvas,file_name)
+        emailer.send_email(dash_info[1])
+        
+    time.sleep(10) # use shorter time for real use
